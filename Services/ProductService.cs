9b4bd2fd1759c5bb1f00b16c7caf7069dcd7a889
Omlegg <Omlegg.Dev@gmail.com>
2025-04-26 18:30:49 +0400

    Sprint 1

---
+++ Services/ProductService.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ScrewItBackEnd.Entities;
+using ScrewItBackEnd.Models;
+using ScrewItBackEnd.Data;
+
+namespace  ScrewItBackEnd.Services
+{
+    public class ProductService
+    {
+        private readonly ScrewItDbContext _context;
+
+    public ProductService(ScrewItDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateProductAsync(string name, string description, decimal price)
+    {
+        var product = new Product
+        {
+            Name = name,
+            Description = description,
+            Price = price
+        };
+
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Product> GetProductByIdAsync(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+
+    public async Task UpdateProductAsync(int id, string name, string description, decimal price)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product != null)
+        {
+            product.Name = name;
+            product.Description = description;
+            product.Price = price;
+            await _context.SaveChangesAsync();
+        }
+    }
+    public async Task DeleteProductAsync(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product != null)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<List<Product>> GetAllProductsAsync()
+    {
+        return _context.Products.ToList();
+    }
+    }
+}
